7e58a1a7295fa00f393f2b6c90f7f3b2449661be
KIMGEEK <hajunbaek025@gmail.com>
2024-07-27 00:34:42 +0900

    Complete index structure in main.js
    Finally, ul part information is update. edit Data folder, index can
    increase or decrease.

--- WEB2 - NodeJs/main.js
+++ WEB2 - NodeJs/main.js
@@ -3,32 +3,24 @@ var fs = require('fs');
 var url = require('url');
 
 var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  var title = queryData.id
 
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-          fs.readdir('./data', function(err, filelist){
-            console.log(filelist);
-            var title = 'Welcome!';
-          var description = 'Hello, NodeJS';  
-        /*
-          var list = `<ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>`
-        */
-       var list ='<ul>';
-       var i = 0;
-       while(i < filelist.length){
-        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        i= i+1;
-       } 
-       list = list +'</ul>';
-          const template = `
+  if(pathname === '/'){
+    if(queryData.id === undefined){
+      fs.readdir('./data', function(err, filelist){
+        var title = 'Welcome!';
+        var description = 'Hello, NodeJS';
+        var list ='<ul>';
+        var i = 0;
+        while(i < filelist.length){
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i= i+1;
+        } 
+        list = list +'</ul>';
+        const template = `
           <!doctype html>
           <html>
           <head>
@@ -42,38 +34,44 @@ var app = http.createServer(function(request,response){
             <p>${description}</p>
           </body>
           </html>
-          `;
-      response.writeHead(200);
-      response.end(template);
-          })
-      } else {
-    fs.readFile(`data/${queryData.id}`,'utf8', (err, description)=>{
-      const template = `
-      <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    <ul>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ul>
-    <h2>${title}</h2>
-    <p>${description}</p>
-  </body>
-  </html>
-  `;
-  response.writeHead(200);
-  response.end(template);
-    });}
+        `;
+        response.writeHead(200);
+        response.end(template);
+      })
+    } else {
+        fs.readdir('./data', function(err, filelist){
+          var list ='<ul>';
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i= i+1;
+          } 
+          list = list +'</ul>';
+          fs.readFile(`data/${queryData.id}`,'utf8', (err, description)=>{
+            var title = queryData.id
+            const template = `
+              <!doctype html>
+              <html>
+              <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+              </head>
+              <body>
+                <h1><a href="/">WEB</a></h1>
+                ${list}
+                <h2>${title}</h2>
+                <p>${description}</p>
+              </body>
+              </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+          });
+        });
+      }
   } else {
     response.writeHead(404);
-  response.end('404 Not Found');
-  }
- 
+    response.end('404 Not Found');
+  } 
 });
 app.listen(3000);
